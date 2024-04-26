1a6e44d7a2571debbc8e7bd9a3080af0a9021d6b
Alejandro Muñoz García <alejandromgarcia90@gmail.com>
2024-04-26 20:54:42 +0200

    commit

--- modelos/album.js
+++ modelos/album.js
@@ -2,10 +2,10 @@ const mongoose=require('mongoose');
 
 var albumSchema=new mongoose.Schema(
     {
-        nombre: {type: String, required:[true,'* Nombre obligatorio'],maxLenght:[50,'*Maximo 50 caracteres']},
-        artista: {type: String, required:[true,'* Artista obligatorio'], maxLenght:[200,'*Maximo 200 caracteres']},
+        nombre: {type: String, required:[true,'* Nombre obligatorio'], maxlenght:[200,'*Maximo 200 caracteres']},
+        artista: {type: String, required:[true,'* Artista obligatorio'], maxlenght:[200,'*Maximo 200 caracteres']},
         anhoLanzamiento: {type: Number, required:[true,'* Año de lanzamiento obligatorio']},
-        genero: {type: String, required:[true,'* Genero obligatorio'], maxLenght:[50,'*Maximo 50 caracteres']},
+        genero: {type: String, required:[true,'* Genero obligatorio'], maxlenght:[50,'*Maximo 50 caracteres']},
         imagenPortada: {type: String},
         numCanciones: {type: Number, required:[true,'* Número de canciones obligatorio']},
         precio: {type: Number, required:[true,'* Precio obligatorio']},
