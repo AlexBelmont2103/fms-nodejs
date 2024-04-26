692c906802eb4674a81d08ff2f05e17d26c4f217
Alejandro Muñoz García <alejandromgarcia90@gmail.com>
2024-04-26 21:34:29 +0200

    commit

--- config_server_express/routingTienda.js
+++ config_server_express/routingTienda.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const tiendaController = require("../controllers/tiendaController");
 router.get("/RecuperarGeneros", tiendaController.recuperarGeneros);
 router.get("/RecuperarAlbumes", tiendaController.recuperarAlbumes);
+router.get("/RecuperarAlbumesGenero/:genero", tiendaController.recuperarAlbumesGenero);
+router.get("/RecuperarAlbumesArtista/:artista", tiendaController.recuperarAlbumesArtista);
 module.exports = router;
--- controllers/tiendaController.js
+++ controllers/tiendaController.js
@@ -1,58 +1,88 @@
 const Genero = require("../modelos/genero");
 const Album = require("../modelos/album");
 module.exports = {
-    recuperarGeneros: async function(req, res) {
-        try {
-            //Recuperamos todos los generos en un array
-            let generos = await Genero.find();
-            res.status(200).send({
-                codigo: 0,
-                mensaje: "Generos recuperados",
-                error: null,
-                otrosdatos: null,
-                datosgeneros: generos,
-            });
-        } catch (error) {
-            res.status(500).send({
-                codigo: 1,
-                mensaje: "Error al intentar recuperar generos",
-                error: error.message,
-                otrosdatos: null,
-                datosgeneros: null,
-            });
-        }
-    },
-    recuperarAlbumes: async function(req, res) {
-        try {
-            //1º vemos si hay algo en el query
-            const genero = req.query.genero;
-            console.log("Genero: ", genero);
-            let albumes;
-            if (genero !== undefined && genero !== null && genero !== ""){
-                console.log("Recuperamos albumes de un genero");
-                //Recuperamos los albumes de un genero
-                albumes = await Album.find({ genero: genero });
-            } else {
-                console.log("Recuperamos todos los albumes");
-                //Recuperamos todos los albumes
-                albumes = await Album.find();
-            }
-            console.log("Albumes recuperados: ", albumes);
-            res.status(200).send({
-                codigo: 0,
-                mensaje: "Albumes recuperados",
-                error: null,
-                otrosdatos: null,
-                datosalbumes: albumes,
-            });
-        } catch (error) {
-            res.status(500).send({
-                codigo: 1,
-                mensaje: "Error al intentar recuperar albumes",
-                error: error.message,
-                otrosdatos: null,
-                datosalbumes: null,
-            });
-        }
-    },
+  recuperarGeneros: async function (req, res) {
+    try {
+      //Recuperamos todos los generos en un array
+      let generos = await Genero.find();
+      res.status(200).send({
+        codigo: 0,
+        mensaje: "Generos recuperados",
+        error: null,
+        otrosdatos: null,
+        datosgeneros: generos,
+      });
+    } catch (error) {
+      res.status(500).send({
+        codigo: 1,
+        mensaje: "Error al intentar recuperar generos",
+        error: error.message,
+        otrosdatos: null,
+        datosgeneros: null,
+      });
+    }
+  },
+  recuperarAlbumes: async function (req, res) {
+    try {
+      albumes = await Album.find();
+      res.status(200).send({
+        codigo: 0,
+        mensaje: "Albumes recuperados",
+        error: null,
+        otrosdatos: null,
+        datosalbumes: albumes,
+      });
+    } catch (error) {
+      res.status(500).send({
+        codigo: 1,
+        mensaje: "Error al intentar recuperar albumes",
+        error: error.message,
+        otrosdatos: null,
+        datosalbumes: null,
+      });
+    }
+  },
+  recuperarAlbumesGenero: async function (req, res) {
+    try {
+      let genero = req.params.genero;
+      let albumes = await Album.find({ genero: genero });
+      res.status(200).send({
+        codigo: 0,
+        mensaje: "Albumes recuperados",
+        error: null,
+        otrosdatos: null,
+        datosalbumes: albumes,
+      });
+    } catch (error) {
+      res.status(500).send({
+        codigo: 1,
+        mensaje: "Error al intentar recuperar albumes",
+        error: error.message,
+        otrosdatos: null,
+        datosalbumes: null,
+      });
+    }
+  },
+  recuperarAlbumesArtista: async function (req, res) {
+    try {
+      let artista = req.params.artista;
+      let albumes = await Album.find({ artista: artista });
+      res.status(200).send({
+        codigo: 0,
+        mensaje: "Albumes recuperados",
+        error: null,
+        otrosdatos: null,
+        datosalbumes: albumes,
+      });
+      console.log("Albumes recuperados: ", albumes);
+    } catch (error) {
+      res.status(500).send({
+        codigo: 1,
+        mensaje: "Error al intentar recuperar albumes",
+        error: error.message,
+        otrosdatos: null,
+        datosalbumes: null,
+      });
+    }
+  },
 };
